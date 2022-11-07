d98ece940ba89f2e951a047b5660a4de3faae49a
Ounza <rmounza@gmail.com>
2022-11-07 19:48:29 +0300

    Allow Headers

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ var instance = new Razorpay({
 });
 
 app.use(cors({origin:"*"}));
+//this part was added in order to try to fix the cors error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
+    next();
+});
 
 app.use(express.json())
 
